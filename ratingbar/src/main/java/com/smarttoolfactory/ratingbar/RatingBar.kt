623828462db144772da149a46b69c7fd0b294235
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-07-24 10:05:47 +0300

    fix #1

--- ratingbar/src/main/java/com/smarttoolfactory/ratingbar/RatingBar.kt
+++ ratingbar/src/main/java/com/smarttoolfactory/ratingbar/RatingBar.kt
@@ -1,19 +1,33 @@
 package com.smarttoolfactory.ratingbar
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
@@ -317,7 +331,7 @@ private fun RatingBarImpl(
         val gestureModifier = Modifier
             .pointerInput(Unit) {
                 val ratingBarWidth = size.width.toFloat()
-                detectDragGestures { change, _ ->
+                detectHorizontalDragGestures { change, _ ->
 
                     val x = change.position.x
                     val newRating = getRatingFromTouchPosition(
@@ -533,7 +547,7 @@ private fun DrawScope.drawRatingPainters(
                 blendMode = BlendMode.SrcIn
             )
 
-            if(shimmerData.drawBorder){
+            if (shimmerData.drawBorder) {
                 for (i in 0 until itemCount) {
 
                     translate(left = (imageWidth * i + space * i), top = 0f) {
@@ -625,7 +639,7 @@ private fun DrawScope.drawRatingImages(
                 blendMode = BlendMode.SrcIn
             )
 
-            if(shimmerData.drawBorder){
+            if (shimmerData.drawBorder) {
                 for (i in 0 until itemCount) {
 
                     translate(left = (imageWidth * i + space * i), top = 0f) {
