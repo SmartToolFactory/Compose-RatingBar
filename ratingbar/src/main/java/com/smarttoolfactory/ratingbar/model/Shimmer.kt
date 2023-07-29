4d428851fa0a0ac9b9a21224c059a2b4364a3177
Smart Tool Factory <smarttoolfactory@icloud.com>
2023-07-29 18:38:38 +0300

    update shimmer data to take fill and border colors

--- ratingbar/src/main/java/com/smarttoolfactory/ratingbar/model/Shimmer.kt
+++ ratingbar/src/main/java/com/smarttoolfactory/ratingbar/model/Shimmer.kt
@@ -1,28 +1,34 @@
 package com.smarttoolfactory.ratingbar.model
 
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import com.smarttoolfactory.ratingbar.DefaultColor
 
 @Immutable
 internal data class ShimmerData(
-    val colors: List<Color>,
+    val fillColors: List<Color>,
     val progress: Float,
-    val drawBorder: Boolean = false
+    val drawBorder: Boolean = false,
+    val borderColors: List<Color>? = null
 )
 
 /**
  * Shimmer for rating items
- * @param colors colors that are displayed on brush
+ * @param fillColors colors that are displayed on brush that fills items
  * @param animationSpec [InfiniteRepeatableSpec] to set animation and repeat modes either repeat
  * or reverse.
  * @param drawBorder when set to true draws empty rating item above shimmer. This looks good
  * with empty items with borders with small stroke widths
+ * @param borderColors colors that drawn as border when [drawBorder] is set true
  */
 @Immutable
 data class Shimmer(
-    val colors: List<Color> = listOf(
+    val fillColors: List<Color> = listOf(
         DefaultColor.copy(alpha = .9f),
         DefaultColor.copy(alpha = .3f),
         DefaultColor.copy(alpha = .9f)
@@ -34,7 +40,8 @@ data class Shimmer(
         ),
         RepeatMode.Reverse
     ),
-    val drawBorder: Boolean = false
+    val drawBorder: Boolean = false,
+    val borderColors: List<Color>? = null
 ) {
 
     companion object {
@@ -50,13 +57,14 @@ data class Shimmer(
             drawBorder: Boolean = false
         ): Shimmer {
             return Shimmer(
-                colors = listOf(
+                fillColors = listOf(
                     color.copy(alpha = .9f),
                     color.copy(alpha = .3f),
                     color.copy(alpha = .9f)
                 ),
                 animationSpec = animationSpec,
-                drawBorder = drawBorder
+                drawBorder = drawBorder,
+                borderColors = null
             )
         }
     }
